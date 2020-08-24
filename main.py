75ef238d4fc8767dfc7d21a93770731fd750e217
suriyakrishnan <suriya@outlook.in>
2020-08-24 14:36:37 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,16 +6,17 @@ screen = cv2.imread('pic2.jpg')
 screen = cv2.cvtColor(screen,cv2.COLOR_BGR2RGB)
 
 #print(pytesseract.image_to_boxes(screen))
-"""height,width,_=screen.shape
-config=r'--oem 2 --psm 6 outputbase digits'
-box=pytesseract.image_to_boxes(screen,config=config)
+height,width,_=screen.shape
+#config=r'--oem 2 --psm 6 outputbase digits'
+box=pytesseract.image_to_boxes(screen)
 for b in box.splitlines():
     b=b.split(' ')
+    print(box)
     x,y,w,h = int(b[1]),int(b[2]),int(b[3]),int(b[4])
     cv2.rectangle(screen,(x,height-y),(w,height-h),(0,0,255),1)
-    cv2.putText(screen,b[0],(x,height-y+20),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),1)"""
+   # cv2.putText(screen,b[0],(x,height-y+20),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),1)
 
-height,width,_=screen.shape
+"""height,width,_=screen.shape
 box=pytesseract.image_to_data(screen)
 print(box)
 for x,b in enumerate(box.splitlines()):
@@ -25,7 +26,7 @@ for x,b in enumerate(box.splitlines()):
         if(len(b)==12):
             x,y,w,h = int(b[6]),int(b[7]),int(b[8]),int(b[9])
             cv2.rectangle(screen,(x,y),(w+x,h+y),(0,0,255),1)
-            cv2.putText(screen,b[11],(x,height-y+20),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),1)
+            cv2.putText(screen,b[11],(x,height-y+20),cv2.FONT_HERSHEY_SIMPLEX,1,(50,50,255),1)"""
 
 
 cv2.imshow('Capture',screen)
